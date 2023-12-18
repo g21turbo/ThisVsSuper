fd6fc9deef99b0760302e4037bef257966425707
g21turbo <dg21stew@gmail.com>
2023-12-17 18:24:31 -0800

    added examples for bad and good constructors

---
+++ src/RectangleBadConstructors.java
@@ -0,0 +1,29 @@
+public class RectangleBadConstructors {
+
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+
+    public RectangleBadConstructors() {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public RectangleBadConstructors(int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public RectangleBadConstructors(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+}
---
+++ src/RectangleGoodConstructors.java
@@ -0,0 +1,31 @@
+public class RectangleGoodConstructors {
+
+    // The 1st constructor calls the 2nd constructor, the second constructor calls the 3rd constructor and the
+    // 3rd constructor initializes the instance variables.
+
+    // The 3rd constructor does all the work.
+
+    // No matter which constructor we call, the variables will always be initialized in the third constructor.
+
+
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+
+    public RectangleGoodConstructors() {
+        this(0,0); // calls the second constructor
+    }
+
+    public RectangleGoodConstructors(int width, int height) {
+        this(0, 0, width, height); //calls the third constructor
+    }
+
+    public RectangleGoodConstructors(int x, int y, int width, int height) {
+        // initializes variables
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
