57cf496c7797bee080f7026d642d6645bc46ace7
g21turbo <dg21stew@gmail.com>
2023-12-17 18:26:35 -0800

    added comments

--- src/RectangleGoodConstructors.java
+++ src/RectangleGoodConstructors.java
@@ -7,6 +7,8 @@ public class RectangleGoodConstructors {
 
     // No matter which constructor we call, the variables will always be initialized in the third constructor.
 
+    // This is known as constructor chaining, the last constructor has the responsibility to initialize the variables.
+
 
     private int x;
     private int y;
